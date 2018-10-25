e3ca3db487ea8f1f79c5b341bcaac1ac8634e6a1
jboeshrt <jboeshart@jenesismarketinggroup.com>
2018-10-25 13:48:58 -0400

    updated app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WaterproofingPage } from '../pages/waterproofing/waterproofing';
+import { FoundationPage } from '..pages/foundation/foundation';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,7 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+	WaterproofingPage,
+	FoundationPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+	WaterproofingPage,
+	FoundationPage
   ],
   providers: [
     StatusBar,
